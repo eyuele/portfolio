93ebcee2b5ffdaa61d12b0c5ff494b40e1389867
eyuele <eyuele4479@gmail.com>
2026-05-24 21:36:28 +0300

    portfolio: NordicFlow Addition

--- NordicFlow/script.js
+++ NordicFlow/script.js
@@ -29,7 +29,7 @@ mobileOverlay.addEventListener('click', closeMenu);
 
 // Close menu when a link is clicked
 mobileLinks.forEach(link => {
-    link.addEventListener('click', closeMenu());
+    link.addEventListener('click', closeMenu);
 });
 
 // Close menu on Escape key press
@@ -38,4 +38,46 @@ document.addEventListener('keydown', (e) => {
         closeMenu();
     }
 });
+
+
+//Email form
+
+emailjs.init({
+    publicKey: "e4biaHHDNicczj0Ki",
+});
+
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const service = document.getElementById('service').value;
+    const date = document.getElementById('date').value;
+    const time = document.getElementById('b-time').value;
+    const message = document.getElementById('message').value;
+
+    const templateParams = {
+        from_name: name,
+        from_email: email,
+        phone: phone,
+        service: service,
+        preferred_date: date,
+        preferred_time: time,
+        message: message,
+    };
+
+    emailjs.send('service_ab7qh9h', 'template_79mmtdr', templateParams)
+        .then(function (response) {
+            alert('Thank you! Your request has been sent.');
+            form.reset();  // clears the form
+        })
+        .catch(function (error) {
+            alert('Failed to send. Please try again later.');
+            console.error(error);
+        });
+});
+
+
 
