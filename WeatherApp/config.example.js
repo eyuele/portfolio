96cc77c398b96a45d5851c764dd27d478d2ca4ab
eyuele <eyuele4479@gmail.com>
2026-08-04 07:42:39 -0400

    update

--- WeatherApp/config.example.js
+++
@@ -1,4 +0,0 @@
-// config.example.js 
-// Update the files and the link accordingly
-
-const API_KEY = "YOUR_API_KEY_HERE";
--- WeatherApp/script.js
+++
@@ -1,4 +0,0 @@
-
-const cityName = document.getElementById("cityName");
-const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`);
-
